461050d0a2b79de1911a7078f1922b19ef77c98b
Georgi Lazarov <lazarovgogo@gmail.com>
2024-06-20 11:31:01 +0300

    Update eks.tf

--- ch8/clusters/tofu/infra/terrafrom/eks/eks.tf
+++ ch8/clusters/tofu/infra/terrafrom/eks/eks.tf
@@ -1,12 +1,25 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.10.0"
+  version = "20.14.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
